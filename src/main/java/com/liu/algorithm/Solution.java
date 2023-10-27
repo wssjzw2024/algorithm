e0598697fad9935209ba4742e2b638c777399c0f
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2023-10-27 16:23:21 +0800

    ZigzaglevelOrder003

--- src/main/java/com/liu/algorithm/Solution.java
+++ src/main/java/com/liu/algorithm/Solution.java
@@ -5,4 +5,16 @@ package com.liu.algorithm;
  */
 public class Solution {
 
+    public int countDigits(int num) {
+        int total = num;
+        int count = 0;
+        while (num != 0){
+            int div =num % 10;
+            if(total%div == 0){
+                count++;
+            }
+            num = num/10;
+        }
+        return count;
+    }
 }
