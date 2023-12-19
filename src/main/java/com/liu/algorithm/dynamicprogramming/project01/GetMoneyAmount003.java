3f83e4099688e097cc4ee8b42f1336467a5c3112
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2023-12-19 11:35:19 +0800

    GetMoneyAmount003

--- src/main/java/com/liu/algorithm/dynamicprogramming/project01/GetMoneyAmount003.java
+++ src/main/java/com/liu/algorithm/dynamicprogramming/project01/GetMoneyAmount003.java
@@ -5,7 +5,7 @@ package com.liu.algorithm.dynamicprogramming.project01;
  */
 public class GetMoneyAmount003 {
     /**
-     * 动态规划
+     * 区间dp
      * 思路
      * 用 f(i,j)表示在范围 [i,j]内确保胜利的最少金额，目标是计算 f(1,n)
      * f(1,n)=x+max(f(1,x−1),f(x+1,n))。
@@ -29,5 +29,41 @@ public class GetMoneyAmount003 {
             }
         }
         return f[1][n];
+    }
+
+    /**
+     * 记忆化搜索
+     * 思路
+     * 比较容易想到的做法为使用「递归」进行求解。
+     * 设计递归函数为 int dfs(int l, int r) 传入参数 l 和 r 代表在范围 [l,r][l, r][l,r] 内进行猜数，返回值为在 [l,r][l, r][l,r] 内猜中数字至少需要多少钱。
+     * 步骤
+     * 1.判断边界
+     * 2.查看缓存是否存在
+     * 3.ans最大值
+     * 4.i从l到r
+     * 5.当前值 去l到i - 1 与 i + 1到r中最大值与i的和
+     * 6.ans 等于 ans与cur的最小值
+     * @param n
+     * @return
+     */
+    public int getMoneyAmount1(int n){
+        int[][] cache = new int[n + 1][n + 1];
+        return dfs(1,n,cache);
+    }
+
+    private int dfs(int l,int r,int[][] cache){
+        if(l >= r){
+            return 0;
+        }
+        if(cache[l][r] != 0){
+            return cache[l][r];
+        }
+        int ans = Integer.MAX_VALUE;
+        for(int i = l; i <= r; i++){
+            int cur = Math.max(dfs(l,i - 1,cache),dfs(i + 1,r,cache)) + i;
+            ans = Math.min(ans,cur);
+        }
+        cache[l][r] = ans;
+        return ans;
     }
 }
