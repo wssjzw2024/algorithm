c403c9cdbfc6c9bc2e31026bc4a49296bffc50cb
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-02-29 14:32:24 +0800

    MergeSort

---
+++ src/main/java/com/liu/algorithm/array/project02/MergeSort.java
@@ -0,0 +1,57 @@
+package com.liu.algorithm.array.project02;
+
+/**
+ * @author liu
+ */
+public class MergeSort {
+
+
+    /**
+     * 归并排序
+     * 思路
+     *  分解、合并
+     * 步骤
+     *  1.递归将数组分解
+     *  2.两两合并
+     * @param nums
+     */
+    public void sort(int[] nums){
+        int len = nums.length;
+        int[] temp = new int[len];
+        decompose(nums,temp,0,len - 1);
+    }
+
+    public void decompose(int[] arr,int[] temp,int left,int right){
+        if(left < right){
+            int mid = (left +right) / 2;
+            decompose(arr, temp, left, mid);
+            decompose(arr,temp,mid + 1,right);
+            merge(arr,temp,left,mid,right);
+        }
+    }
+
+    public void merge(int[] arr,int[] temp,int left,int right,int mid){
+        int i = 0;
+        int m = left;
+        int n = mid + 1;
+
+        while (m <= mid && n <= right){
+            if(arr[m] < arr[n]){
+                temp[i++] = arr[m++];
+            }else {
+                temp[i++] = arr[n++];
+            }
+        }
+        while (m <= mid){
+            temp[i++] = arr[m++];
+        }
+        while (n <= right){
+            temp[i++] = arr[n++];
+        }
+        for (int j = 0; j < i; j++) {
+            arr[left + j] = temp[j];
+        }
+    }
+
+
+}
--- src/main/java/com/liu/algorithm/dynamicprogramming/project03/LengthOfLIS001.java
+++ src/main/java/com/liu/algorithm/dynamicprogramming/project03/LengthOfLIS001.java
@@ -36,4 +36,45 @@ public class LengthOfLIS001 {
         return dp[len - 1];
     }
 
+    /**
+     * 贪心 + 二分查找
+     * 思路
+     *  如果我们要使上升子序列尽可能的长，则我们需要让序列上升得尽可能慢，因此我们希望每次在上升子序列最后加上的那个数尽可能的小。
+     * 步骤
+     *  1.初始化 len = 1，数组长度
+     *  2.数组d  d[len] = nums[0];
+     *  3.循环
+     *  4.当nums[i] 大于d[len] 数组增加
+     *  5.当nums[i] 小于d[len] 找到最接近
+     * @param nums
+     * @return
+     */
+    public int lengthLIS1(int[] nums){
+        int len = 1 ,n = nums.length;
+        if(n == 0){
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+
+        for (int i = 1; i < n; ++i){
+            if(nums[i] > d[len]){
+                d[++len] = nums[i];
+            }else {
+                int l = 1,r = len,pos = 0;
+                while (l <= r){
+                    int mid = (l + r) >> 1;
+                    if(d[mid] < nums[i]){
+                        pos = mid;
+                        l = mid + 1;
+                    }else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+
 }
