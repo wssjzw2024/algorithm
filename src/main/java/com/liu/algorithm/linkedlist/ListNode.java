efb540559aeed1c49af89220956dcee79cda9636
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-03-08 11:17:57 +0800

    DeleteDuplicates001

---
+++ src/main/java/com/liu/algorithm/linkedlist/ListNode.java
@@ -0,0 +1,21 @@
+package com.liu.algorithm.linkedlist;
+
+/**
+ * @author liu
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
