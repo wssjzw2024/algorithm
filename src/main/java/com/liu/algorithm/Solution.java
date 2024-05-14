c8123cb8e8e7095286574545e26948e4da2d39c0
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-05-14 09:11:55 +0800

    remake

--- src/main/java/com/liu/algorithm/Solution.java
+++ src/main/java/com/liu/algorithm/Solution.java
@@ -1,42 +1,11 @@
 package com.liu.algorithm;
 
-import java.util.Stack;
 
 /**
  * @author liu
  */
 public class Solution {
+    public static void main(String[] args) {
 
-    class TreeNode{
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(){}
-        TreeNode(int val){
-            this.val = val;
-        }
-        TreeNode(int val,TreeNode left,TreeNode right){
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    public TreeNode inverTree(TreeNode root){
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.isEmpty()){
-            TreeNode temp = stack.pop();
-            if(temp.left != null){
-                stack.push(temp.left);
-            }
-            if(temp.right != null){
-                stack.push(temp.right);
-            }
-            TreeNode left = temp.left;
-            temp.left = temp.right;
-            temp.right = left;
-        }
-        return root;
     }
 }
