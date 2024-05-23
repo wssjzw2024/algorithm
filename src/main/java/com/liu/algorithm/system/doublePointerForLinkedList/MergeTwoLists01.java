0cf41228920bfe02f1b06c7ffcd9f6df1dcd7922
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-05-23 17:51:29 +0800

    mergetwolist

---
+++ src/main/java/com/liu/algorithm/system/doublePointerForLinkedList/MergeTwoLists01.java
@@ -0,0 +1,34 @@
+package com.liu.algorithm.system.doublePointerForLinkedList;
+
+import com.liu.algorithm.ListNode;
+
+/**
+ * @author liu
+ */
+public class MergeTwoLists01 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        ListNode dumy = new ListNode(-1);
+        ListNode current = dumy;
+        while (list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            }else {
+                current.next = list2;
+                list2=list2.next;
+            }
+            current = current.next;
+        }
+        if(list1 != null) {
+            current.next = list1;
+        }
+        if(list2 !=null){
+            current.next = list2;
+        }
+        return dumy.next;
+    }
+}
