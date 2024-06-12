dcc36cad98422aece9c5d96e90186248ae560e8d
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-06-12 08:51:57 +0800

    detectCycle

---
+++ src/main/java/com/liu/algorithm/system/doublePointerForLinkedList/DetectCycle06.java
@@ -0,0 +1,31 @@
+package com.liu.algorithm.system.doublePointerForLinkedList;
+
+import com.liu.algorithm.ListNode;
+
+/**
+ * @author liu
+ */
+public class DetectCycle06 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
+}
