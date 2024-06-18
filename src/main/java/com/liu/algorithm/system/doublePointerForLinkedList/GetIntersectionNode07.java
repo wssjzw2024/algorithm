aea577c2e5b33a2e50871b34323f472ca7f72988
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-06-18 08:44:49 +0800

    getIntersectionNode

---
+++ src/main/java/com/liu/algorithm/system/doublePointerForLinkedList/GetIntersectionNode07.java
@@ -0,0 +1,29 @@
+package com.liu.algorithm.system.doublePointerForLinkedList;
+
+import com.liu.algorithm.ListNode;
+
+/**
+ * @author liu
+ */
+public class GetIntersectionNode07 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        while (tempA != tempB) {
+            if (tempA == null) {
+                tempA = headB;
+            }else {
+                tempA = tempA.next;
+            }
+
+            if (tempB == null) {
+                tempB = headA;
+            }else {
+                tempB = tempB.next;
+            }
+        }
+        return tempA;
+    }
+}
