475d9f1f98aa4ceae68cdd4c946618fa2dc0342f
刘剑 <liujian@liujiandeMacBook-Pro.local>
2024-06-21 16:05:23 +0800

    isValid

--- src/main/java/com/liu/algorithm/system/datastructure/stack/IsValid01.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/IsValid01.java
@@ -6,6 +6,19 @@ import java.util.Stack;
  * @author liu
  */
 public class IsValid01 {
+    /**
+     * 步骤
+     * 1.将字符串转为字符数组
+     * 2.判断长度是否是偶数
+     * 3.循环字符数组
+     * 4.判断是否是左括号，是加入栈
+     * 5.如果是右括号，栈弹出相应的括号
+     * 6.如何栈为空，返回false
+     * 7.循环结束
+     * 8.如果栈为空，返回true
+     * @param s
+     * @return
+     */
     public boolean isValid(String s) {
         char[] chars = s.toCharArray();
         if(chars.length %2 !=0) return false;
