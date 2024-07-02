57073f80bc9fcb5f03bd92b8733871f3dc39c1ef
刘剑 <11015171+gsdks@user.noreply.gitee.com>
2024-07-02 08:57:56 +0800

    InsertionSort

--- src/main/java/com/liu/algorithm/system/sort/InsertionSort.java
+++ src/main/java/com/liu/algorithm/system/sort/InsertionSort.java
@@ -10,6 +10,10 @@ public class InsertionSort {
          * 3-n
          */
         for (int i = 1; i < arr.length; i++) {
+            /*
+                0-0有序
+                0-1有序
+             */
             for (int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
                 swap(arr, j, j-1);
             }
