7d480148bcdfb7c8e6aa93911965e06942bfb364
刘剑 <liujian@liujiandeMacBook-Pro.local>
2024-07-16 18:41:56 +0800

    ShellSort

--- src/main/java/com/liu/algorithm/system/datastructure/stack/DailyTemperatures05.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/DailyTemperatures05.java
@@ -1,7 +1,12 @@
 package com.liu.algorithm.system.datastructure.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class DailyTemperatures05 {
     public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
         return null;
     }
 }
--- src/main/java/com/liu/algorithm/system/sort/ShellSort.java
+++ src/main/java/com/liu/algorithm/system/sort/ShellSort.java
@@ -4,8 +4,11 @@ public class ShellSort {
 
     public  void sort(int[] arr) {
         int len = arr.length;
+        // 分组 最初是长度一半，到最后为1
         for (int gap = len / 2; gap > 0; gap /= 2) {
+            // 从gap开始，到len
             for (int i = gap; i < len; i++) {
+                //组中，和前面一个比较
                 for (int j = i - gap; j >= 0 && arr[j] < arr[j -gap]; j -= gap) {
                         swap(arr, j, j - gap);
                 }
