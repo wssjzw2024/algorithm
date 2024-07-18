08e84b54da1c225d93ee4b37d2f31b50c8a43669
刘剑 <liujian@liujiandeMacBook-Pro.local>
2024-07-18 14:43:14 +0800

    dailyTemperatures

--- src/main/java/com/liu/algorithm/system/datastructure/stack/DailyTemperatures05.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/DailyTemperatures05.java
@@ -1,12 +1,35 @@
 package com.liu.algorithm.system.datastructure.stack;
 
-import java.util.ArrayDeque;
+
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class DailyTemperatures05 {
+    /**
+     * 单调栈
+     * 步骤
+     * 1，获取数组长度
+     * 2. 新建结果数组
+     * 3. 新建单调栈
+     * 4. 循环数组
+     * 5.   循环判断，栈是否为空，当前下标元素是否大于栈顶下标元素，
+     * 6.   大于则，弹出栈顶下表，赋值给结果数组
+     * 7. 将当前下标加入单调栈
+     * 8.返回结果数组
+     * @param temperatures
+     * @return
+     */
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        return null;
+        int len = temperatures.length;
+        int[] res = new int[len];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < len; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
     }
 }
