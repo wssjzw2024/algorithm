c98b6e97bc8145e98245e04e333bd6c0c3980abf
刘剑 <wssjzw_lj@163.com>
2024-07-25 09:02:35 +0800

    NextGreaterElement07

---
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/monotonicstack/NextGreaterElement07.java
@@ -0,0 +1,7 @@
+package com.liu.algorithm.system.datastructure.stack.monotonicstack;
+
+public class NextGreaterElement07 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        return null;
+    }
+}
