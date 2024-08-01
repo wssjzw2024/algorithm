5a313dcebe9ab5a682d69a4d5b0569f6e7c5872e
刘剑 <liujian@liujiandeMacBook-Pro.local>
2024-08-01 11:29:39 +0800

    NextGreaterElement07

--- src/main/java/com/liu/algorithm/system/datastructure/stack/EvalRPN06.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/EvalRPN06.java
@@ -4,6 +4,16 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class EvalRPN06 {
+    /**
+     * 步骤
+     * 1.新建一个栈
+     * 2.循环遍历tokens数组
+     * 3.如果token是运算符，则弹出栈顶两个元素，运算，并将结果压入栈中
+     * 4.如果token是数字，则直接压入栈中
+     * 5.返回栈顶元素
+     * @param tokens
+     * @return
+     */
     public int evalRPN(String[] tokens) {
         Deque<Integer> stack = new ArrayDeque<>();
         for (String token : tokens) {
--- src/main/java/com/liu/algorithm/system/datastructure/stack/monotonicstack/NextGreaterElement07.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/monotonicstack/NextGreaterElement07.java
@@ -6,15 +6,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NextGreaterElement07 {
+    /**
+     * 步骤
+     * 1. 创建一个栈
+     * 2.新建一个map
+     * 3.新建一个数组
+     * 4.倒序遍历nums2
+     * 5.如果栈不为空，并且栈顶元素小于当前元素，则出栈，直到栈为空或者栈顶元素大于当前元素
+     * 6.如果栈为空，则将当前元素放入map中，key为当前元素，value为-1
+     * 7.如果栈不为空，则将当前元素放入map中，key为当前元素，value为栈顶元素
+     * 9.循环遍历nums1，将nums1中的元素放入res中，res[i] = map.get(nums1[i])
+     * 10.返回res
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Deque<Integer> stack = new ArrayDeque<>();
         Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[nums1.length];
         for (int i = 0; i < nums2.length; i++) {
-            while (!stack.isEmpty() && nums2[stack.peek()] > nums2[i]) {
+            while (!stack.isEmpty() && nums2[stack.peek()] < nums2[i]) {
                 stack.pop();
             }
-            stack.push(i);
+            map.put(nums2[i], stack.isEmpty() ? -1 : nums2[stack.peek()]);
+            stack.push(nums2[i]);
         }
-        return null;
+        for (int i = 0; i < nums1.length; i++){
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
     }
 }
