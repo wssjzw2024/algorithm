8c92a7a9257b2bc5821da8aba02e363e9d92fe93
刘剑 <wssjzw_lj@163.com>
2024-08-07 08:57:38 +0800

    NextGreaterElements08

--- src/main/java/com/liu/algorithm/system/datastructure/stack/monotonicstack/NextGreaterElements08.java
+++ src/main/java/com/liu/algorithm/system/datastructure/stack/monotonicstack/NextGreaterElements08.java
@@ -1,8 +1,22 @@
 package com.liu.algorithm.system.datastructure.stack.monotonicstack;
 
-public class NextGreaterElements08 {
-    public int[] nextGreaterElements(int[] nums) {
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 
-        return null;
+public class NextGreaterElements08 {
+
+    public int[] nextGreaterElements(int[] nums) {
+        int len = nums.length;
+        int[] ret = new int[len];
+        Arrays.fill(ret, -1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < len * 2 -1 ; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                ret[stack.pop()] = nums[i%len];
+            }
+            stack.push(i%len);
+        }
+        return ret;
     }
 }
