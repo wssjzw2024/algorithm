ac9538dbb89bf489bd067ecfcefdf8cc4ea81c41
刘剑 <liujian@liujiandeMacBook-Pro.local>
2024-09-02 11:59:14 +0800

    修改

--- src/main/java/com/liu/algorithm/Solution.java
+++ src/main/java/com/liu/algorithm/Solution.java
@@ -5,8 +5,5 @@ package com.liu.algorithm;
  * @author liu
  */
 public class Solution {
-    public static void main(String[] args) {
-        String str = "I like java";
-        System.out.println(str.codePointAt(2));
-    }
+
 }
